764d21034418ca1e0e765e622f183f0a17b3137c
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-05-11 16:02:11 +0530

    Add Firebase to project

--- app/src/main/java/com/puldroid/whatsappclone/ChatActivity.kt
+++ app/src/main/java/com/puldroid/whatsappclone/ChatActivity.kt
@@ -1,9 +1,11 @@
 package com.puldroid.whatsappclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ChatActivity : AppCompatActivity() {
+//    private lateinit var keyboardVisibilityHelper: KeyboardVisibilityUtil
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)

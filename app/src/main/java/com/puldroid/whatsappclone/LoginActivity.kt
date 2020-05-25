680c86a5bebfd9d92359c0370df9a3bbc50bbbcf
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-05-25 12:44:23 +0530

    Add LoginActivity.kt

---
+++ app/src/main/java/com/puldroid/whatsappclone/LoginActivity.kt
@@ -0,0 +1,82 @@
+package com.puldroid.whatsappclone
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var phoneNumber: String
+    private lateinit var countryCode: String
+    private lateinit var alertDialogBuilder: MaterialAlertDialogBuilder
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        phoneNumberEt.addTextChangedListener {
+            nextBtn.isEnabled = !(it.isNullOrEmpty() || it.length < 10)
+        }
+        //Hint Request - Assignment
+        nextBtn.setOnClickListener {
+            checkNumber()
+        }
+
+    }
+
+    private fun checkNumber() {
+        countryCode = ccp.selectedCountryCodeWithPlus
+        phoneNumber = countryCode + phoneNumberEt.text.toString()
+
+        if (validatePhoneNumber(phoneNumberEt.text.toString())) {
+            notifyUserBeforeVerify(
+                "We will be verifying the phone number:$phoneNumber\n" +
+                        "Is this OK, or would you like to edit the number?"
+            )
+        } else {
+            toast("Please enter a valid number to continue!")
+        }
+    }
+
+
+
+
+    private fun validatePhoneNumber(phone: String): Boolean {
+        if (phone.isEmpty()) {
+            return false
+        }
+        return true
+    }
+
+    private fun notifyUserBeforeVerify(message: String) {
+        alertDialogBuilder = MaterialAlertDialogBuilder(this)
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("Ok") { _, _ ->
+            showLoginActivity()
+        }
+
+        alertDialogBuilder.setNegativeButton("Edit") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        alertDialogBuilder.setCancelable(false)
+        alertDialogBuilder.create()
+        alertDialogBuilder.show()
+    }
+
+
+    private fun showLoginActivity() {
+        startActivity(
+            Intent(this, OtpActivity::class.java).putExtra("phoneNumber", phoneNumber)
+        )
+        finish()
+    }
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
---
+++ app/src/main/java/com/puldroid/whatsappclone/OtpActivity.kt
@@ -0,0 +1,11 @@
+package com.puldroid.whatsappclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}

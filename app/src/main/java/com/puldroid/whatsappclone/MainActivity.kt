998ad524ae73bff143a4e958d350063af534e440
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-03 13:14:34 +0530

    add toolbar and setup viewpager

--- app/src/main/java/com/puldroid/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/puldroid/whatsappclone/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.puldroid.whatsappclone
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import com.puldroid.whatsappclone.adapters.ScreenSlidePagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        viewPager.adapter = ScreenSlidePagerAdapter(this)
+        TabLayoutMediator(tabs, viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.text = "CHATS"
+                    }
+                    1 -> {
+                        tab.text = "PEOPLE"
+                    }
+                }
+            }).attach()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/com/puldroid/whatsappclone/adapters/ScreenSlidePagerAdapter.kt
@@ -0,0 +1,14 @@
+package com.puldroid.whatsappclone.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.puldroid.whatsappclone.fragments.ChatsFragment
+
+class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    private val NUM_PAGES: Int = 2
+
+    override fun getItemCount(): Int = NUM_PAGES
+
+    override fun createFragment(position: Int): Fragment = ChatsFragment()
+}

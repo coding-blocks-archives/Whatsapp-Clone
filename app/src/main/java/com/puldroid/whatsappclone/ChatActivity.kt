f0c83fc9f527d550d2813b815dfad531fae33c0b
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-06-15 13:26:57 +0530

    add chat layout

--- app/src/main/java/com/puldroid/whatsappclone/ChatActivity.kt
+++ app/src/main/java/com/puldroid/whatsappclone/ChatActivity.kt
@@ -5,8 +5,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.puldroid.whatsappclone.utils.KeyboardVisibilityUtil
+import com.squareup.picasso.Picasso
+import com.vanniktech.emoji.EmojiManager
+import com.vanniktech.emoji.google.GoogleEmojiProvider
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 const val USER_ID = "userId"
 const val USER_THUMB_IMAGE = "thumbImage"
@@ -14,14 +23,80 @@ const val USER_NAME = "userName"
 
 class ChatActivity : AppCompatActivity() {
 
+    private val friendId: String by lazy {
+        intent.getStringExtra(USER_ID)
+    }
+    private val name: String by lazy {
+        intent.getStringExtra(USER_NAME)
+    }
+    private val image: String by lazy {
+        intent.getStringExtra(USER_THUMB_IMAGE)
+    }
+    private val mCurrentUid: String by lazy {
+        FirebaseAuth.getInstance().uid!!
+    }
+    private val db: FirebaseDatabase by lazy {
+        FirebaseDatabase.getInstance()
+    }
+
     private lateinit var keyboardVisibilityHelper: KeyboardVisibilityUtil
     private val mutableItems: MutableList<ChatEvent> = mutableListOf()
     private val mLinearLayout: LinearLayoutManager by lazy { LinearLayoutManager(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EmojiManager.install(GoogleEmojiProvider())
+
         setContentView(R.layout.activity_chat)
         keyboardVisibilityHelper = KeyboardVisibilityUtil(root) {
+
+        }
+
+        nameTv.text = name
+        Picasso.get().load(image).into(userImgView)
+
+        swipeToLoad.setOnRefreshListener {
+            val workerScope = CoroutineScope(Dispatchers.Main)
+            workerScope.launch {
+                delay(2000)
+                swipeToLoad.isRefreshing = false
+            }
+        }
+
+
+        sendBtn.setOnClickListener {
+            msgEdtv.text?.let {
+                if (it.isNotEmpty()) {
+                    sendMessage(it.toString())
+                    it.clear()
+                }
+            }
+        }
+    }
+
+    private fun sendMessage(msg: String) {
+        val id = getMessages(friendId).push().key
+        checkNotNull(id) { "Cannot be null" }
+        getMessages(friendId).child(id).setValue(Message(msg, mCurrentUid, id))
+            .addOnSuccessListener {
+
+            }
+        updateLastMessage(msg, mCurrentUid, friendId)
+    }
+
+    private fun updateLastMessage(msg: String, mCurrentUid: String, friendId: String) {
+
+    }
+
+
+    private fun getMessages(friendId: String) = db.reference.child("messages/${getId(friendId)}")
+
+
+    private fun getId(friendId: String): String {
+        return if (friendId > mCurrentUid) {
+            mCurrentUid + friendId
+        } else {
+            friendId + mCurrentUid
         }
     }
 

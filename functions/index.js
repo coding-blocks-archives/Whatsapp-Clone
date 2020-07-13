19175a12af543e53f62cfb7533f0eae826e7aa77
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2020-07-13 19:54:10 +0530

    add sample code for trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
  // Create and Deploy Your First Cloud Functions
  // https://firebase.google.com/docs/functions/write-firebase-functions
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+ exports.sendNotification = functions.database.ref('/messages/{chatId}/{msgId}')
+    .onCreate((snapshot, context) => {
+    const chat_id = context.params.chatId;
+    const messageFrom = snapshot.val().senderId;
+    const user_id = chat_id.replace(messageFrom,'');
+   const status =  admin.firestore().collection("users").doc(user_id).get().then(doc => {
+    const token = doc.data().deviceToken;
+        console.log(token);
+        return true;
+    })
+    return status;
+    })
